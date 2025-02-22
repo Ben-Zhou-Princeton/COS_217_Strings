d629e7a653a8ec3996a3c5a3c66980a5c6be4ec1
Ben Zhou <bz2883@princeton.edu>
2025-02-22 18:05:00 -0500

    Commit

---
+++ str.h
@@ -0,0 +1,10 @@
+#ifndef STR_INCLUDED
+#define STR_INCLUDED
+
+int Str_getLength(const char *str);
+char *Str_copy(char *dest, const char *src); 
+char *Str_concat(char *dest, const char *src);
+int Str_compare(const char *str1, const char *str2);
+char *Str_search(const char *haystack, const char *needle); 
+
+#endif 
