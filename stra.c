70c42f85ac3fd50fa5a321f61c19deae91e6f75d
Ben Zhou <bz2883@princeton.edu>
2025-02-23 13:05:05 -0500

    Commit

--- stra.c
+++ stra.c
@@ -15,7 +15,7 @@ char *Str_copy(char dest[], const char src[])
 {   
     size_t uLength = 0; 
     assert(src != NULL && dest != NULL);
-    while(src[uLength != '\0']) {
+    while(src[uLength] != '\0') {
         dest[uLength] = src[uLength];
         uLength++; 
     }
