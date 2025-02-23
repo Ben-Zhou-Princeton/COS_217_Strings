f023423c0e03d10fd08bb609605c20e029723e76
Ben Zhou <bz2883@princeton.edu>
2025-02-23 15:17:04 -0500

    Commit

--- teststr.c
+++ teststr.c
@@ -520,8 +520,8 @@ static void testSearch(void)
       const char acNeedle[] = "d";
       pcResult1 = Str_search(acHaystack, acNeedle);
       pcResult2 = strstr(acHaystack, acNeedle);
-      printf("%s\n", pcResult1);
-      printf("%s\n", pcResult2);
+      /*printf("%c\n", pcResult1); */
+      printf("%c\n", pcResult2);
       ASSURE(pcResult1 == pcResult2);
    }
 
